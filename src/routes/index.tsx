d7dcec59ec9f249f52d611f42be3a067994da5b3
jihun1233 <leejihun1233@gmail.com>
2022-05-31 17:00:48 +0900

    Update: app에 sidebar, header 추가

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,17 +6,31 @@ import HomePage from './Home'
 import UserManage from './UserManage'
 import UserInfo from './UserInfo'
 import NotFoundPage from './NotFoundPage'
+import Sidebar from './Sidebar'
+import Header from './Header'
+import Breadcrumb from './Breadcrumb'
 
 const App = () => {
   return (
     <div className={styles.app}>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='login' element={<LoginPage />} />
-        <Route path='userManage' element={<UserManage />} />
-        <Route path='userInfo' element={<UserInfo />} />
-        <Route path='*' element={<NotFoundPage />} />
-      </Routes>
+      <Header />
+      <main>
+        <div className={styles.sidebarContainer}>
+          <Sidebar />
+        </div>
+        <div className={styles.pageContainer}>
+          <Breadcrumb />
+          <div className={styles.pageContents}>
+            <Routes>
+              <Route path='/' element={<HomePage />} />
+              <Route path='login' element={<LoginPage />} />
+              <Route path='userManage' element={<UserManage />} />
+              <Route path='userInfo' element={<UserInfo />} />
+              <Route path='*' element={<NotFoundPage />} />
+            </Routes>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
