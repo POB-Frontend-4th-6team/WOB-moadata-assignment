5e1417862a39e6dea844044a190aed28817c9a1d
youngman <sksn12@naver.com>
2022-05-31 15:42:57 +0900

    pull받기전 commit

--- src/routes/UserInfo/HeartRate/index.tsx
+++ src/routes/UserInfo/HeartRate/index.tsx
@@ -1,7 +1,29 @@
 import styles from './heartRate.module.scss'
+import { VictoryChart, VictoryLine, VictoryTheme } from 'victory'
+import jsonData from 'assets/jsons/heartrate_136_0226_유령회원1번.json'
 
 const HeartRate = () => {
-  return <div className={styles.container}>HeartRate</div>
+  return (
+    <div className={styles.container}>
+      <VictoryChart theme={VictoryTheme.material}>
+        <VictoryLine
+          style={{
+            data: { stroke: '#c43a31' },
+            parent: { border: '1px solid #ccc' },
+          }}
+          range={[60, 150]}
+          interpolation='natural'
+          data={[
+            { x: 1, y: 70 },
+            { x: 2, y: 70 },
+            { x: 3, y: 80 },
+            { x: 4, y: 100 },
+            { x: 5, y: 120 },
+          ]}
+        />
+      </VictoryChart>
+    </div>
+  )
 }
 
 export default HeartRate
--- src/routes/UserInfo/index.tsx
+++ src/routes/UserInfo/index.tsx
@@ -1,7 +1,12 @@
 import styles from './userInfo.module.scss'
+import HeartRate from './HeartRate/index'
 
 const UserInfo = () => {
-  return <div className={styles.container}>UserInfo</div>
+  return (
+    <div className={styles.container}>
+      <HeartRate />
+    </div>
+  )
 }
 
 export default UserInfo
