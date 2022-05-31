840e756fcc498d1ad2aee2e050d86061463ecbd8
jihun1233 <leejihun1233@gmail.com>
2022-06-01 01:29:14 +0900

    Update: Sidebar나타나기 toggle -> hover로 변경
    - 페이지 부분은 더이상 밀려나지 않음

--- src/routes/Layout/index.tsx
+++ src/routes/Layout/index.tsx
@@ -4,21 +4,12 @@ import styles from './layout.module.scss'
 import Sidebar from './Sidebar'
 import Header from './Header'
 import Breadcrumb from './Breadcrumb'
-import { useState } from 'react'
-import { cx } from 'styles'
 
 const Layout = () => {
-  const [showSidebar, setShowSidebar] = useState(false)
-  const toggleSidebarOpen = () => {
-    setShowSidebar((prev) => !prev)
-  }
   return (
     <div className={styles.app}>
       <Header />
-      <main className={cx({ [styles.showSidebar]: showSidebar })}>
-        <button className={styles.sidebarButton} type='button' onClick={toggleSidebarOpen}>
-          사이드바 열기
-        </button>
+      <main>
         <div className={styles.sidebarContainer}>
           <Sidebar />
         </div>
