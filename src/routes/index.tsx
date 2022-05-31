ac3b01dedfa3d619e8dd6a2ccf42b3b1518f73e6
jihun1233 <leejihun1233@gmail.com>
2022-05-31 17:12:05 +0900

    Add: Layout 컴포넌트 생성, App부분 outlet 적용

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,37 +1,23 @@
 import { Routes, Route } from 'react-router-dom'
 
-import styles from './routes.module.scss'
 import LoginPage from './Login'
 import HomePage from './Home'
 import UserManage from './UserManage'
 import UserInfo from './UserInfo'
 import NotFoundPage from './NotFoundPage'
-import Sidebar from './Sidebar'
-import Header from './Header'
-import Breadcrumb from './Breadcrumb'
+import Layout from './Layout'
 
 const App = () => {
   return (
-    <div className={styles.app}>
-      <Header />
-      <main>
-        <div className={styles.sidebarContainer}>
-          <Sidebar />
-        </div>
-        <div className={styles.pageContainer}>
-          <Breadcrumb />
-          <div className={styles.pageContents}>
-            <Routes>
-              <Route path='/' element={<HomePage />} />
-              <Route path='login' element={<LoginPage />} />
-              <Route path='userManage' element={<UserManage />} />
-              <Route path='userInfo' element={<UserInfo />} />
-              <Route path='*' element={<NotFoundPage />} />
-            </Routes>
-          </div>
-        </div>
-      </main>
-    </div>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route path='' element={<HomePage />} />
+        <Route path='login' element={<LoginPage />} />
+        <Route path='userManage' element={<UserManage />} />
+        <Route path='userInfo' element={<UserInfo />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   )
 }
 
