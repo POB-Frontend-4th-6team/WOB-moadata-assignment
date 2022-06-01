062ec3289b217fc846f58f065ad66630c18a05a5
Chiho <minepanorama@naver.com>
2022-06-01 18:49:14 +0900

    ADD: 회원 검색 결과 Dummy Data로 구현

--- src/routes/UserManage/Result/index.tsx
+++ src/routes/UserManage/Result/index.tsx
@@ -1,7 +1,52 @@
 import styles from './result.module.scss'
 
-const Result = () => {
-  return <div className={styles.container}>Result</div>
+import { useNavigate } from 'react-router-dom'
+
+interface Props {
+  data: {
+    registerNumber: number
+    registerDate: string
+    loginId: string
+    gender: string
+    birthday: string
+  }
+}
+
+const Result = ({ data }: Props) => {
+  const navigate = useNavigate()
+
+  const setUserAtomAndNavigate = () => {
+    navigate('/userInfo')
+  }
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>회원번호</th>
+          <th>가입일</th>
+          <th>로그인 ID</th>
+          <th>성별</th>
+          <th>생년월일</th>
+          <th>상세</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr className={styles.tableBody}>
+          <td>{data.registerNumber}</td>
+          <td>{data.registerDate}</td>
+          <td>{data.loginId}</td>
+          <td>{data.gender}</td>
+          <td>{data.birthday}</td>
+          <td>
+            <button type='button' onClick={setUserAtomAndNavigate}>
+              관리
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  )
 }
 
 export default Result
--- src/routes/UserManage/index.tsx
+++ src/routes/UserManage/index.tsx
@@ -1,7 +1,16 @@
 import styles from './userManage.module.scss'
+import Result from './Result'
+
+const DATAS = [
+  { registerNumber: 1, registerDate: '2018.03.17', loginId: 'iamchho', gender: '남', birthday: '1995.12.01' },
+  { registerNumber: 2, registerDate: '2018.10.21', loginId: 'rowastudy', gender: '여', birthday: '1997.1.20' },
+  { registerNumber: 3, registerDate: '2018.08.16', loginId: 'choco123', gender: '남', birthday: '1997.3.17' },
+]
 
 const UserManage = () => {
-  return <div className={styles.container}>UserManage</div>
+  const Results: JSX.Element[] = DATAS.map((data) => <Result data={data} key={data.loginId} />)
+
+  return <div className={styles.container}>{Results}</div>
 }
 
 export default UserManage
