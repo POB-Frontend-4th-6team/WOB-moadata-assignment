06b68b4a1f4f0837e3c8eff1d7dfc17900bb1b49
Jihyuk Ma <t90laser93@gmail.com>
2022-06-02 01:45:50 +0900

    FEATURE: Search 컴포넌트 기본 구조 생성
    -

--- src/routes/UserManage/Search/index.tsx
+++ src/routes/UserManage/Search/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useRecoilState } from 'recoil'
 import { Button } from 'routes/_components/Button'
+import SearchDateRange from 'routes/_components/SearchDateRange'
 
 import { searchedDateRangeState } from 'states/userSearch'
 
@@ -9,25 +10,19 @@ import styles from './search.module.scss'
 const Search = () => {
   const [isDatePickerModalOpen, setIsDatePickerModalOpen] = useState(false)
   const [searchedDateRange, setSearchedDateRange] = useRecoilState(searchedDateRangeState)
+  const [weeks, setWeeks] = useState(['', ''])
 
   return (
     <form className={styles.container}>
+      {/* 로그인ID / 회원번호 중에 택일하도록 할 드롭다운 입니다 */}
       <div className={styles.userInfoInputsContainer}>
-        <label htmlFor='userId'>로그인 ID</label>
+        <div className={styles.dropDown}>드롭다운</div>
         <input id='userId' type='text' />
-        <label htmlFor='userNumber'>회원 번호</label>
-        <input id='userNumber' type='text' />
       </div>
-      <div className={styles.dateRangeInputContainer}>
-        <label htmlFor='searchRange'>조회기간</label>
-        <input id='searchRange' type='text' /> ~
-        <input id='searchRange' type='text' />
-        <Button size='large'>오늘</Button>
-        <Button size='large'>일주일</Button>
-        <Button size='large' primary>
-          전체
-        </Button>
+      <div className={styles.dateRange}>
+        <SearchDateRange weeks={weeks} setWeeks={setWeeks} />
       </div>
+
       <div className={styles.formButtonsContainer}>
         <Button size='large'>초기화</Button>
         <Button size='large' primary>
--- src/routes/UserManage/index.tsx
+++ src/routes/UserManage/index.tsx
@@ -1,9 +1,11 @@
+import Search from './Search'
 import styles from './userManage.module.scss'
 
 const UserManage = () => {
   return (
     <section className={styles.container}>
       <h2>UserManage</h2>
+      <Search />
     </section>
   )
 }
