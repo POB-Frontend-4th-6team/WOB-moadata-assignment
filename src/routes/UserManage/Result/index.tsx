2222b37383c16fc1839ad8fde4435f33cdfde890
Chiho <minepanorama@naver.com>
2022-06-01 23:16:13 +0900

    FIX: 테이블 태그 결과 컴포넌트에서 제거

--- src/routes/UserManage/Result/index.tsx
+++ src/routes/UserManage/Result/index.tsx
@@ -21,32 +21,18 @@ const Result = ({ data }: Props) => {
   }
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>회원번호</th>
-          <th>가입일</th>
-          <th>로그인 ID</th>
-          <th>성별</th>
-          <th>생년월일</th>
-          <th>상세</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr className={styles.tableBody}>
-          <td>{data.member_seq}</td>
-          <td>{data.registered_date}</td>
-          <td>{data.user_id}</td>
-          <td>{data.gender}</td>
-          <td>{data.birth}</td>
-          <td>
-            <button type='button' onClick={setUserAtomAndNavigate}>
-              관리
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <tr className={styles.tableBody}>
+      <td>{data.member_seq}</td>
+      <td>{data.registered_date}</td>
+      <td>{data.user_id}</td>
+      <td>{data.gender}</td>
+      <td>{data.birth}</td>
+      <td>
+        <button type='button' onClick={setUserAtomAndNavigate}>
+          관리
+        </button>
+      </td>
+    </tr>
   )
 }
 
--- src/routes/UserManage/index.tsx
+++ src/routes/UserManage/index.tsx
@@ -10,12 +10,27 @@ const UserManage = () => {
   useMount(() => {
     const searchedMemberList = getMemberInfo({ id: undefined, number: 136, startDate: 20220401, endDate: 20220514 })
     console.log('searchedMemberList', searchedMemberList)
-    setMember(searchedMemberList)
   })
 
   const Results: JSX.Element[] = member.map((data) => <Result data={data} key={`userId_${data.nickname}`} />)
 
-  return <div className={styles.container}>{Results}</div>
+  return (
+    <div className={styles.container}>
+      <table>
+        <thead>
+          <tr>
+            <th>회원번호</th>
+            <th>가입일</th>
+            <th>로그인 ID</th>
+            <th>성별</th>
+            <th>생년월일</th>
+            <th>상세</th>
+          </tr>
+        </thead>
+        <tbody>{Results}</tbody>
+      </table>
+    </div>
+  )
 }
 
 export default UserManage
